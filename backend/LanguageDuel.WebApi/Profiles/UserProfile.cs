aa54274fa4e2533951868434a629dc29b241ef6e
Nikita Titarenko <nikitatitarenko81@gmail.com>
2026-03-03 12:12:13 +0200

    feat: add user profile for auto mapper

---
+++ backend/LanguageDuel.WebApi/Profiles/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using LanguageDuel.Application.Dtos.Users;
+using LanguageDuel.WebApi.Requests.Users;
+
+namespace LanguageDuel.WebApi.Profiles;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<RegisterUserRequestModel, RegisterUserDto>();
+        CreateMap<ConfirmEmailDto, EmailConfirmationRequestModel>();
+        CreateMap<EmailConfirmationRequestModel, ConfirmEmailDto>();
+        CreateMap<LoginRequestModel, LoginUserDto>();
+    }
+}
