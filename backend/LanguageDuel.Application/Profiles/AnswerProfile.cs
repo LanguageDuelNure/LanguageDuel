7bdb56b337a5c4b521847d67fda54954938d865f
Nikita Titarenko <nikitatitarenko81@gmail.com>
2026-03-15 08:43:09 +0200

    feat: add answer profile

---
+++ backend/LanguageDuel.Application/Profiles/AnswerProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using LanguageDuel.Application.Dtos.Answers;
+using LanguageDuel.Domain.Entities;
+
+namespace LanguageDuel.Application.Profiles;
+
+public class AnswerProfile : Profile
+{
+    public AnswerProfile()
+    {
+        CreateMap<Answer, AnswerDto>();
+    }
+}
