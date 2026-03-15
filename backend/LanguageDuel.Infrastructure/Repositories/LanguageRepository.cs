c6d43b7b9ae7c89099b04d950ebff489970482e9
Nikita Titarenko <nikitatitarenko81@gmail.com>
2026-03-15 18:26:18 +0200

    feat: add language repository

---
+++ backend/LanguageDuel.Infrastructure/Repositories/LanguageRepository.cs
@@ -0,0 +1,16 @@
+﻿using LanguageDuel.Application.Repositories;
+using LanguageDuel.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanguageDuel.Infrastructure.Repositories;
+
+public class LanguageRepository(ApplicationDbContext dbContext) : Repository<Language>(dbContext), ILanguageRepository
+{
+    public async Task<IEnumerable<Language>> GetLanguagesWithRatingAsync(Guid userId)
+    {
+        return await DbSet
+            .Include(l => l.ApplicationUserLanguages
+                .Where(aul => aul.ApplicationUserId == userId))
+            .ToListAsync();
+    }
+}
