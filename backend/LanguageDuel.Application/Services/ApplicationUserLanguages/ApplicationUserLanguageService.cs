738dada22a412b2af6a7b3a3fa9f2ac07067d417
Nikita Titarenko <nikitatitarenko81@gmail.com>
2026-03-15 18:23:58 +0200

    feat: add application user language service

---
+++ backend/LanguageDuel.Application/Services/ApplicationUserLanguages/ApplicationUserLanguageService.cs
@@ -0,0 +1,38 @@
+﻿using LanguageDuel.Application.Dtos.Results;
+using LanguageDuel.Application.Repositories;
+using LanguageDuel.Domain.Entities;
+
+namespace LanguageDuel.Application.Services.ApplicationUserLanguages;
+
+public class ApplicationUserLanguageService(IUnitOfWork unitOfWork, IRepository<ApplicationUserLanguage> applicationUserLanguageRep) : IApplicationUserLanguageService
+{
+    public async Task<Result> ChangeUsersRatingAsync(Guid userId, Guid languageId, int ratingChange)
+    {
+        var applicationUserLanguage = await applicationUserLanguageRep.GetAsync(userId, languageId);
+
+        if (applicationUserLanguage == null)
+        {
+            applicationUserLanguageRep.Add(new ApplicationUserLanguage
+            {
+                ApplicationUserId = userId,
+                LanguageId = languageId,
+                Rating = ratingChange < 0 ? 0 : ratingChange,
+            });
+        }
+        else
+        {
+            if (applicationUserLanguage.Rating + ratingChange < 0)
+            {
+                applicationUserLanguage.Rating = 0;
+            }
+            else
+            {
+                applicationUserLanguage.Rating += ratingChange;
+            }
+        }
+
+        await unitOfWork.CommitAsync();
+
+        return new Result();
+    }
+}
---
+++ backend/LanguageDuel.Application/Services/ApplicationUserLanguages/IApplicationUserLanguageService.cs
@@ -0,0 +1,9 @@
+﻿using LanguageDuel.Application.Dtos.Games;
+using LanguageDuel.Application.Dtos.Results;
+
+namespace LanguageDuel.Application.Services.ApplicationUserLanguages;
+
+public interface IApplicationUserLanguageService
+{
+    Task<Result> ChangeUsersRatingAsync(Guid userId, Guid languageId, int ratingChange);
+}
