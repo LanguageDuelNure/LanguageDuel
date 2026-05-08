875be94276668b1b5bd0fd7a2200ff8ce1b38a7b
Nikita Titarenko <nikitatitarenko81@gmail.com>
2026-05-08 18:06:22 +0300

    refactor: put SenderName into const variable

--- backend/LanguageDuel.Infrastructure/Services/SmtpEmailSender.cs
+++ backend/LanguageDuel.Infrastructure/Services/SmtpEmailSender.cs
@@ -8,13 +8,15 @@ namespace LanguageDuel.Infrastructure.Services;
 
 public class SmtpEmailSender(IOptions<SmtpEmailOptions> options) : IEmailSender
 {
+    private const string SenderName = "LanguageDuel";
+    
     private readonly SmtpEmailOptions _options = options.Value;
 
     public async Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
-        MimeMessage message = new();
+        var message = new MimeMessage();
 
-        message.From.Add(new MailboxAddress("LanguageDuel", _options.Email));
+        message.From.Add(new MailboxAddress(SenderName, _options.Email));
 
         message.To.Add(new MailboxAddress(string.Empty, email));
 
@@ -25,7 +27,7 @@ public class SmtpEmailSender(IOptions<SmtpEmailOptions> options) : IEmailSender
             Text = htmlMessage
         };
 
-        using SmtpClient smtp = new();
+        using var smtp = new SmtpClient();
         await smtp.ConnectAsync(_options.Host, _options.Port, MailKit.Security.SecureSocketOptions.StartTls);
         await smtp.AuthenticateAsync(_options.Email, _options.Password);
         await smtp.SendAsync(message);
